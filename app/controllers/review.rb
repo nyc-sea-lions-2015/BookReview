b8f31d4c7e0ffdbe4975d19e8c8a5545075f649a
Eveanandi Butler <butler.eveanandi@gmail.com>
2015-04-01 12:16:17 -0400

    Complete new review(reflection) form)

--- app/controllers/review.rb
+++ app/controllers/review.rb
@@ -1,3 +1,8 @@
 get '/review/new' do
   erb :'review/new'
 end
+
+post 'review/new' do
+  @new_post = Post.new(book_title: params[:book_title], author: params[:author])
+
+end
--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -4,9 +4,9 @@ get '/user/:id' do
 end
 
 post '/user/new' do
-  @new_user = User.create(name: params[:name], password: params[:password], email: params[:email])
+  @new_user = User.new(name: params[:name], password: params[:password], email: params[:email])
 
-  if @new_user
+  if @new_user.save!
     session[:user_id] = @new_user.id
     redirect "/user/#{@new_user.id}"
   else
