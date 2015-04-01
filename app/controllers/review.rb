74b43b30e527ade17765b8049a1cddc4e2745f40
Eveanandi Butler <butler.eveanandi@gmail.com>
2015-04-01 12:32:42 -0400

    Finish review new post route and create review show view

--- app/controllers/review.rb
+++ app/controllers/review.rb
@@ -2,7 +2,17 @@ get '/review/new' do
   erb :'review/new'
 end
 
-post 'review/new' do
-  @new_post = Post.new(book_title: params[:book_title], author: params[:author])
+post '/review/new' do
+  @new_post = Review.new(book_title: params[:book_title], author: params[:author], start_date: params[:start_date], finish_date: params[:finish_date], review_content: params[:review_content], book_rating: params[:book_rating], user_id: current_user.id)
 
+  if @new_post.save!
+    redirect "/review/#{@new_post.id}"
+  else
+    redirect "/review/new"
+  end
+
+end
+
+get '/review/:id' do
+  erb :'review/show'
 end
