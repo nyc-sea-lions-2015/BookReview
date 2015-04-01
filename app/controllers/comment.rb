544d32ef0fcdd1407673c5762af759a336a981ad
Eveanandi Butler <butler.eveanandi@gmail.com>
2015-04-01 15:27:43 -0400

    Create edit and deletes for comments

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -3,15 +3,35 @@ get '/review/:id/comment/_new' do
   erb :"comment/_new"
 end
 
+get '/comment/:id/edit' do
+  @current_comment = Comment.find_by(id: params[:id])
+  erb :"comment/edit"
+end
+
+put '/comment/:id' do
+  @existing_comment = Comment.find_by(id: params[:id])
+  if @existing_comment
+    @existing_comment.content = params[:content]
+    @existing_comment.save!
+    redirect "/review/#{@existing_comment.review.id}"
+  else
+     [500, 'something went wrong']
+  end
+end
+
 post '/review/:id/comment' do
- # p params
-
   @new_comment = Comment.new(content: params[:content], review_id: params[:id], user_id: current_user.id)
-
   if @new_comment.save!
     redirect "/review/#{@new_comment.review.id}"
   else
     redirect '/review/:id/comment/_new'
   end
+end
+
+delete '/review/:id/comment/:comment_id' do
+  @existing_comment = Comment.find_by(id: params[:comment_id])
+  @existing_comment.destroy
+  redirect "review/#{params[:id]}"
+
 
 end
